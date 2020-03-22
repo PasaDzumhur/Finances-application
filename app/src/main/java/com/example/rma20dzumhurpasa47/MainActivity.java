04f788fe824fa5db6c6bc7bfa5d00a286e5764eb
PasaDzumhur <pdzumhur1@etf.unsa.ba>
2020-03-23 00:46:42 +0100

    Initial commit

---
+++ app/src/main/java/com/example/rma20dzumhurpasa47/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.rma20dzumhurpasa47;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
