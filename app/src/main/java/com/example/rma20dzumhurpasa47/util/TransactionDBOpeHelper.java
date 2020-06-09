0bc18d58b32d3d8665b2f80d4cfff8c9179d5147
PasaDzumhur <pdzumhur1@etf.unsa.ba>
2020-06-09 02:18:02 +0200

    da ne bude da nista ne radim, samo sto nista ne radi

---
+++ app/src/main/java/com/example/rma20dzumhurpasa47/util/TransactionDBOpeHelper.java
@@ -0,0 +1,55 @@
+package com.example.rma20dzumhurpasa47.util;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class TransactionDBOpeHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "TransactionDataBase.db";
+    public static final int DATABASE_VERSION = 2;
+
+
+    public TransactionDBOpeHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    public TransactionDBOpeHelper(Context context){
+        super(context,DATABASE_NAME,null,DATABASE_VERSION);
+    }
+
+    public static final String TRANSACTION_TABLE = "transactions";
+    public static final String TRANSACTION_ID = "id";
+    public static final String TRANSACTION_TYPE_ID = "typeID";
+    public static final String TRANSACTION_TITLE = "title";
+    public static final String TRANSACTION_AMOUNT = "amount";
+    public static final String TRANSACTION_DATE = "date";
+    public static final String TRANSACTION_INTERVAL = "transactionInterval";
+    public static final String TRANSACTION_END_DATE = "transactionEndDate";
+    public static final String TRANSACTION_ITEM_DESCRIPTION = "itemDescription";
+    public static final String TRANSACTION_TABLE_CREATE=
+            "CREATE TABLE IF NOT EXISTS " + TRANSACTION_TABLE+" (" +
+                    TRANSACTION_ID + " INTEGER PRIMARY KEY, " +
+                    TRANSACTION_TITLE + " TEXT NOT NULL, " +
+                    TRANSACTION_TYPE_ID + " INTEGER NOT NULL, "+
+                    TRANSACTION_DATE + " TEXT NOT NULL, " +
+                    TRANSACTION_AMOUNT + " INTEGER NOT NULL, "+
+                    TRANSACTION_INTERVAL + " INTEGER, "+
+                    TRANSACTION_ITEM_DESCRIPTION + "TEXT, "+
+                    TRANSACTION_END_DATE + "TEXT);";
+    private static final String TRANSACTION_DROP = "DROP TABLE IF EXISTS " + TRANSACTION_TABLE;
+
+
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TRANSACTION_TABLE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
